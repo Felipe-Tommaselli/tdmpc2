eda888c856b56d3cf2049a439c4e5eda2058ee5c
Felipe-Tommaselli <f.tommaselli@usp.br>
2024-07-22 18:10:11 -0300

    run command

--- tdmpc2/train.sh
+++ tdmpc2/train.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-python3 train.py 2>&1 | grep -v -E "XML Attribute\[version\] in element\[sdf\] not defined in SDF, ignoring.| TIFFFetchNormalTag: Warning, ASCII value for tag \"Artist\" does not end in null byte. | TIFFFetchNormalTag: Warning, Incompatible type for \"RichTIFFIPTC\"; tag ignored.|TIFFFieldWithTag: Internal error, unknown tag"
-
+python3 train.py 2>&1 | grep -v -E "XML Attribute\[version\] in element\[sdf\] not defined in SDF, ignoring\.|TIFFFetchNormalTag: Warning, ASCII value for tag \"Artist\" does not end in null byte\.|TIFFFetchNormalTag: Warning, Incompatible type for \"RichTIFFIPTC\"; tag ignored\.|TIFFFieldWithTag: Internal error, unknown tag"
