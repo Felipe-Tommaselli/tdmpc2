d583ce5cb54cfbe5867a39664015926afac18d2b
Felipe-Tommaselli <f.tommaselli@usp.br>
2025-01-05 23:21:26 -0300

    organizing repo: files delete

--- tdmpc2/train.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export WANDB_API_KEY="561533083875fbadcc988503797f0285532b9f05"
-python3 train.py 2>&1 | grep -v -E "XML Attribute\[version\] in element\[sdf\] not defined in SDF, ignoring\.|TIFFFetchNormalTag: Warning, ASCII value for tag \"Artist\" does not end in null byte\.|TIFFFetchNormalTag: Warning, Incompatible type for \"RichTIFFIPTC\"; tag ignored\.|TIFFFieldWithTag: Internal error, unknown tag"
